3c8ee2cb6e7afbc0541d94181f043bdd2e74113b
Khandakar Rashed Hassan <kh.rashed900@gmail.com>
2021-09-01 01:49:09 +0600

    Added Constants and Venues class. Added GenerateUrl method in Venus model.

---
+++ TravelRecordApp/TravelRecordApp/TravelRecordApp/Models/Venues.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelRecordApp.Helpers;
+
+namespace TravelRecordApp.Models
+{
+    public class Venues
+    {
+        public static string GenerateUrl(double latitude, double longitude)
+        {
+            return string.Format(Constants.VENUE_BASE_URL, latitude, longitude, Constants.VENUE_CLIENT_ID, Constants.VENUE_CLIENT_SECRET);
+        }
+    }
+}
